def80eed84f641fe18190b05fbf55ce47504d448
Enzo dos Anjos <enzodosanjos10@gmail.com>
2025-04-03 14:57:30 -0300

    Enviar modificações

--- sintaxe.java
+++ sintaxe.java
@@ -1,5 +1,68 @@
+class Variaveis{
+     //Variáveis: int e (short ou long int),float e (long float), double, boolean e a STRING é uma classe.
+    public static void variaveisfun(){
+    
+    System.out.println("---------------- Variáveis --------------------");
+    
+    int numeroInteiro = 10;
+    float numeroFloat = 10;
+    double numeroDouble = 1000000000;
+    boolean resposta = true;
+    String nome = "Enzo";
+ 
+    System.out.println("Numero inteiro: " + numeroInteiro + "\nNumero decimal: " + numeroFloat + "\nNumero double: " + numeroDouble + "\nVariável booleana: " + resposta + "\nString nome: " + nome);
+    
+    System.out.println();
+    }
+}
+
+class Casting{
+
+    public static void tiposDeCasting(){
+    //Casting Implicito:
+    System.out.println("---------------- Casting --------------------");
+    System.out.println("Casting Implícito: ");
+    
+    int numeroInteiro = 10;
+    double numeroDouble = numeroInteiro;
+
+    System.out.println("Número Inteiro: " + numeroInteiro + "\nNúmero Double: " + numeroDouble);
+    System.out.println();
+
+    //Casting Explícito:
+    System.out.println("Casting Explícito: ");
+    
+    double numeroDoubl = 10.20;
+	int numeroInt = (int )numeroDoubl;
+
+    System.out.println("Número Double: " + numeroDoubl + "\nNúmero Inteiro: " + numeroInt);
+    
+    System.out.println();
+    }
+}
+
+class ExplicaçãoString{
+    
+}
+
+
+
 public class sintaxe {
     public static void main (String[] args){
-        System.out.println("Vamo que vamo!");
+
+    Variaveis.variaveisfun();
+    Casting.tiposDeCasting();
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
