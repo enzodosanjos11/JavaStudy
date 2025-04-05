d97ec01f922335f12dde3c57a683320af9bdf59e
Enzo dos Anjos <enzodosanjos10@gmail.com>
2025-04-05 14:49:00 -0300

    Exercício 4 e 5

---
+++ exercicio04.java
@@ -0,0 +1,29 @@
+public class exercicio04 {
+    public static void main(String[] args) {
+        
+        var scanner = new java.util.Scanner(System.in);
+        int multa;
+        int velocidadePermitida = 90;
+        
+        System.out.println("Você passou por um radar, o qual registra sua velocidade e emiti a multa caso seja necessário: ");
+        
+        System.out.println("Digite a velocidade em que o carro estava: ");
+        int velocidadeVeiculo = scanner.nextInt();
+        
+        int velocidadeConsiderada = velocidadeVeiculo + 7;
+
+        System.out.println("Velocidade PERMITIDA: " + velocidadePermitida);
+        
+        if(velocidadeConsiderada > 90){
+            multa = (velocidadeConsiderada - 90) * 10;
+            System.out.println("Devido a velocidade que o veículo estava, você, condutor, receberá uma multa de R$" + multa);
+        }else{
+            System.out.println("Você etá liberado e sem multa, pois estava trafegando dentro do limite de velocidade.");
+        }
+    
+    
+    
+    
+    
+    }
+}
---
+++ exercicio05.java
@@ -0,0 +1,20 @@
+public class exercicio05 {
+    public static void main(String[] args) {
+        var scanner = new java.util.Scanner(System.in);
+        
+        System.out.println("Digite um numero de 1 a 7 e lhe retornaremos o dia correspondente a ele: ");
+        int numero = scanner.nextInt();
+
+        switch(numero){
+            case 1 -> System.out.println("Dia = Domingo.");
+            case 2 -> System.out.println("Dia = Segunda - Feira.");
+            case 3 -> System.out.println("Dia = Terça - Feira.");
+            case 4 -> System.out.println("Dia = Quarta - Feira.");
+            case 5 -> System.out.println("Dia = Quinta - Feira.");
+            case 6 -> System.out.println("Dia = Sexta - Feira.");
+            case 7 -> System.out.println("Dia = Sábado.");
+            default -> System.out.println("Opção Inválida.");
+        }
+    
+    }
+}
